593cf36bad69ec877e884787f16728d067a5a624
Luan Santos <github@luan.sh>
2023-05-01 20:15:57 -0700

    feat(heal friend): knight synergy

--- data-otservbr-global/scripts/spells/healing/heal_friend.lua
+++ data-otservbr-global/scripts/spells/healing/heal_friend.lua
@@ -15,8 +15,28 @@ combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
+	local party = creature:getParty()
+	local synergies = {
+		knight = false,
+	}
+	if party and party:isSharedExperienceEnabled() then
+		if party:hasKnight() then
+			synergies.knight = true
+		end
+	end
+
+	local groupCooldown = 1000
+	if synergies.knight then
+		groupCooldown = 800
+	end
 	creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
-	return combat:execute(creature, variant)
+	if combat:execute(creature, variant) then
+		local condition = Condition(CONDITION_SPELLGROUPCOOLDOWN, CONDITIONID_DEFAULT, 258)
+		condition:setTicks((groupCooldown) / configManager.getFloat(configKeys.RATE_SPELL_COOLDOWN))
+		creature:addCondition(condition)
+		return true
+	end
+	return false
 end
 
 spell:name("Heal Friend")
@@ -25,7 +45,7 @@ spell:group("healing")
 spell:vocation("druid;true", "elder druid;true")
 spell:id(84)
 spell:cooldown(1000)
-spell:groupCooldown(1000)
+spell:groupCooldown(0) -- group cooldown is calculated on the casting
 spell:level(18)
 spell:mana(120)
 spell:needTarget(true)
