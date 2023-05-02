d21e8e62f461b0d33d8dd10d510be2bee19a7ff2
Luan Santos <github@luan.sh>
2023-05-01 21:15:29 -0700

    feat(protector): druid synergy

--- data-otservbr-global/scripts/spells/support/protector.lua
+++ data-otservbr-global/scripts/spells/support/protector.lua
@@ -2,9 +2,11 @@ local combat = Combat()
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_GREEN)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
+local duration = 13000
+
 local skill = Condition(CONDITION_ATTRIBUTES)
 skill:setParameter(CONDITION_PARAM_SUBID, 5)
-skill:setParameter(CONDITION_PARAM_TICKS, 13000)
+skill:setParameter(CONDITION_PARAM_TICKS, duration)
 skill:setParameter(CONDITION_PARAM_SKILL_SHIELDPERCENT, 220)
 skill:setParameter(CONDITION_PARAM_BUFF_DAMAGEDEALT, 65)
 skill:setParameter(CONDITION_PARAM_BUFF_DAMAGERECEIVED, 85)
@@ -14,6 +16,36 @@ combat:addCondition(skill)
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
+	local party = creature:getParty()
+	local synergies = {
+		druid = false,
+	}
+	if party and party:isSharedExperienceEnabled() then
+		if party:hasDruid() then
+			synergies.druid = true
+		end
+	end
+
+	if synergies.druid then
+		local protectParty = Condition(CONDITION_ATTRIBUTES)
+		protectParty:setParameter(CONDITION_PARAM_SUBID, 5)
+		protectParty:setParameter(CONDITION_PARAM_TICKS, duration)
+		protectParty:setParameter(CONDITION_PARAM_BUFF_DAMAGERECEIVED, 98)
+		protectParty:setParameter(CONDITION_PARAM_BUFF_SPELL, true)
+		local members = {party:getLeader()}
+
+		for _, member in ipairs(party:getMembers()) do
+			table.insert(members, member)
+		end
+
+		for _, member in ipairs(members) do
+			if creature:getId() ~= member:getId() then
+				member:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
+				member:addCondition(protectParty)
+			end
+		end
+	end
+
 	if creature:getCondition(CONDITION_ATTRIBUTES, CONDITIONID_COMBAT, 5) then
 		creature:removeCondition(CONDITION_ATTRIBUTES, CONDITIONID_COMBAT, 5)
 	end
