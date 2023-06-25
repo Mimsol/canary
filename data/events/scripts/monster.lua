92114e4152e6821ac13ec01c58e2a8ac4116893c
Luan Santos <git@luan.sh>
2023-06-24 17:44:17 -0700

    feat(elysiera): nerf luck progression

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -19,9 +19,10 @@ local function checkItemType(itemId)
 	return false
 end
 
-function calculateLuckExp(chance)
-	local exp =  math.floor(250 * 0.98 ^ chance + 0.5)
-	return exp
+function calculateLuckExp(chance, experience)
+	local exp =  math.floor(250 * 0.985 ^ chance + 0.5)
+	local multiplier = experience / 2500
+	return exp * multiplier
 end
 
 function Monster:onDropLoot(corpse)
@@ -136,7 +137,7 @@ function Monster:onDropLoot(corpse)
 				for i, loot in pairs(monsterLoot) do
 					local item = corpse:createLootItem(monsterLoot[i], charmBonus, preyLootPercent / 100)
 					if item and #item > 0 then
-						luckExp = luckExp + calculateLuckExp(monsterLoot[i].chance)
+						luckExp = luckExp + calculateLuckExp(monsterLoot[i].chance, mType:experience())
 					end
 					if not item then
 						Spdlog.warn(string.format("[3][Monster:onDropLoot] - Could not add loot item to monster: %s, from corpse id: %d.", self:getName(), corpse:getId()))
