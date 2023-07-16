eae0b30b8c1086badb7133d600c7b2f262d0e9e8
Luan Santos <git@luan.sh>
2023-07-15 23:40:17 -0700

    fxip vip

--- data-otservbr-global/scripts/globalevents/online_points.lua
+++ data-otservbr-global/scripts/globalevents/online_points.lua
@@ -1,35 +1,46 @@
 local config = {
 	enabled = true,
-	checkDuplicateIps = true,
-	showMsgToFree = true,
 	storage = Storage.OnlinePoints.Gain,
+	checkDuplicateIps = false,
+	showMsgToFree = true,
 
-	interval = 1000 * 60 * 6,
+	interval = 60 * 1000,
 
-	coinsFree = 5,
-	coinsVip	= 10,
+	-- per hour | system will calculate how many coins will be given and when
+	coinsFree = 50,
+	coinsVip = 100,
 }
 
 local onlineCoinsEvent = GlobalEvent("GainCoinInterval")
-local intervalSeconds = config.interval / 1000
+local runsPerHour = 3600 / (config.interval / 1000)
+
+local function coinsPerRun(coinsPerHour)
+	return coinsPerHour / runsPerHour
+end
 
 function onlineCoinsEvent.onThink(interval)
-	if not config.enabled then return true end
+	if not configManager.getBoolean(configKeys.VIP_SYSTEM_ENABLED) then
+		return false
+	end
 
 	local players = Game.getPlayers()
-	if #players == 0 then return true end
+	if #players == 0 then
+		return true
+	end
 
+	local checkIp = {}
 	for _, player in pairs(players) do
-			local seconds = math.max(0, player:getStorageValue(config.storage))
-			if seconds >= intervalSeconds then
-				local coins = player:isVip() and config.coinsVip or config.coinsFree
-				player:addTibiaCoins(coins, true)
-				player:sendTextMessage(MESSAGE_STATUS_SMALL, "You have received " .. coins .. " online points.")
-
-				player:setStorageValue(config.storage, 0)
-				return true
+		local ip = player:getIp()
+		if ip ~= 0 and (not config.checkDuplicateIps or not checkIp[ip]) then
+			checkIp[ip] = true
+			local remainder = math.max(0, player:getStorageValue(config.storage)) / 10000000
+			local coins = coinsPerRun(player:isVip() and config.coinsVip or config.coinsFree) + remainder
+			if coins >= 1 then
+				player:addTibiaCoins(math.floor(coins), true)
+				player:sendTextMessage(MESSAGE_STATUS_SMALL, "You have received " .. math.floor(coins) .. " online points.")
 			end
-			player:setStorageValue(config.storage, seconds + math.ceil(interval / 1000))
+			player:setStorageValue(config.storage, (coins - math.floor(coins)) * 10000000)
+		end
 	end
 	return true
 end
